cd5341dd35e4f6d6f0e505193150beb1877544f1
roshanjena4 <jayesh.patil@differenzsystem.com>
2025-09-24 18:40:31 +0530

    Implemented add salary flow

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -6,28 +6,40 @@ export async function POST(request: Request) {
   const { fromAccount, toAccount, amount, category, description } = body;
   const safeToAccount = toAccount && typeof toAccount === "string" && toAccount.trim() !== "" ? toAccount : null;
   try {
-    if(safeToAccount && fromAccount === safeToAccount) {
+    if (safeToAccount && fromAccount === safeToAccount) {
       return new Response(JSON.stringify({ error: 'Cannot transfer to the same account.' }));
     }
-    
+
     if (safeToAccount) {
       const status = await connectionPool.query(
         'select status from accounts where accountnumber = $1',
         [safeToAccount]
       );
-      if(status.rows[0].status === 'blocked')
-      {
+      if (status.rows[0].status === 'blocked') {
         return new Response(JSON.stringify({ success: false, message: 'Transaction declined: recipient account is currently blocked.' }));
       }
       await connectionPool.query(
         'UPDATE accounts SET balance = balance + $1 WHERE accountnumber = $2',
         [amount, safeToAccount]
       );
-      
-      await connectionPool.query(  
+
+      await connectionPool.query(
         'INSERT INTO transactions (from_account_id,to_account_id, amount, category, description, transaction_type,status) VALUES ($1, $2, $3, $4, $5, $6, $7)',
         [safeToAccount, fromAccount, amount, category, description, 'credit', 'Success']
       );
+    }
+    if (category === 'Salary') {
+      const salaryStatus = await connectionPool.query(
+        'INSERT INTO transactions (from_account_id,to_account_id, amount, category, description, transaction_type,status) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        [fromAccount, safeToAccount, amount, category, description, 'credit', 'Success']
+      );
+      const updateAccount = await connectionPool.query(
+        'UPDATE accounts SET balance = balance + $1 WHERE accountnumber = $2',
+        [amount, fromAccount]
+      );
+
+      // if(result.)
+      return new Response(JSON.stringify({ success: true, message: 'Funds received successfully.' }), { status: 201 });
     }
     const result = await connectionPool.query(
       'INSERT INTO transactions (from_account_id,to_account_id, amount, category, description, transaction_type,status) VALUES ($1, $2, $3, $4, $5, $6, $7)',
